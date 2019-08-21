df4737d9397e0458ebe2fda5a8dc6b70bc4cd5dc
alfieqashwa <alfieqashwa@gmail.com>
2019-08-21 20:46:49 +0700

    initial commit

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { funk } from '@theme-ui/presets';
 import celloworld from './celloworld.png';
 
 export default function Logo(props) {
   return (
-    <img
-      width="50"
-      height="50"
-      fill={'primary'}
-      src={celloworld}
-      alt="celloworld"
-    />
+    <>
+      <img
+        width="50"
+        height="50"
+        fill={'primary'}
+        src={celloworld}
+        alt="celloworld"
+      />
+      <div>CELLOWORLD</div>
+    </>
   );
 }
